8d159a7f3065806c1441b595e56b16e1a22112a3
MichalJAsik <michjas000@gmail.com>
2022-08-06 19:08:37 +0200

    v0.0.1
    first step

---
+++ src/Library.java
@@ -0,0 +1,11 @@
+public class Library {
+    public static void main(String[] args) {
+        final String appName="Bibloteka v0.1";
+
+        String title="W pustyni i w puszczy";
+        String author ="Henryk Sienkiwicz";
+        int page=200;
+        String publisher = "Greg";
+        String isbn="17273971;";
+    }
+}
