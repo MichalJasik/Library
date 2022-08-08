336053ec3f6392df988c1cb8871474d25cd28ced
MichalJAsik <michjas000@gmail.com>
2022-08-08 18:57:05 +0200

    0.02
    new class

---
+++ src/Book.java
@@ -0,0 +1,7 @@
+public class Book {
+    String title;
+    String author;
+    int page;
+    String publisher;
+    String isbn;
+}
--- src/Library.java
+++ src/Library.java
@@ -1,11 +1,20 @@
 public class Library {
     public static void main(String[] args) {
-        final String appName="Bibloteka v0.1";
+        final String appName = "Bibloteka v0.1";
+        Book b1 = new Book();
 
-        String title="W pustyni i w puszczy";
-        String author ="Henryk Sienkiwicz";
-        int page=200;
-        String publisher = "Greg";
-        String isbn="17273971;";
+        b1.author = "Henryk Sienkiwicz";
+        b1.title = "W pustyni i w puszczy";
+        b1.page = 200;
+        b1.publisher = "Greg";
+        b1.isbn = "17273971";
+
+        String b1info = "Autor: " + b1.author +
+                "\tTytuł: " + b1.title +
+                "\tStron: " + b1.page +
+                "\tPublikacja: " + b1.publisher +
+                "\tisbn: " + b1.isbn;
+
+        System.out.println(b1info);
     }
 }
