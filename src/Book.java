cdc3ccb716dae344f037bf32aa51feb5bad8d2af
MichalJAsik <michjas000@gmail.com>
2022-08-11 14:12:39 +0200

    0.4

--- src/Book.java
+++ src/Book.java
@@ -5,14 +5,17 @@ public class Book {
     int page;
     String publisher;
     String isbn;
-
     public Book(String title, String author, int realiseDate, int page, String publisher, String isbn) {
+        this( title, author, realiseDate, page, publisher);
+        this.isbn = isbn;
+    }
+
+    Book(String title, String author, int realiseDate, int page, String publisher){
         this.title = title;
         this.author = author;
         this.realiseDate = realiseDate;
         this.page = page;
         this.publisher = publisher;
-        this.isbn = isbn;
     }
 
     void getInfo(){
--- src/Library.java
+++ src/Library.java
@@ -1,12 +1,14 @@
 public class Library {
     public static void main(String[] args) {
+        final String  appName= "Version 0.3 ";
+
         Book b1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010,
                 296, "Greg", "9788373271890");
         Book b2 = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch",
                 2009, 352, "Helion", "9788324620845");
         Book b3 = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide",
                 "Bert Bates, Katherine Sierra", 2008, 851,
-                "McGraw-Hill Osborne Media", "9780071591065");
+                "McGraw-Hill Osborne Media");
 
         b1.getInfo();
         b2.getInfo();
