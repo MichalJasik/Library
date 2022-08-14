e3ff276023592f680f66e2c63651380ce1d08115
MichalJAsik <michjas000@gmail.com>
2022-08-14 14:40:57 +0200

    0.7

--- src/pl/michaljasik/library/app/Library.java
+++ src/pl/michaljasik/library/app/Library.java
@@ -4,18 +4,20 @@ import pl.michaljasik.library.model.Book;
 
 public class Library {
     public static void main(String[] args) {
-        final String  appName= "Version 0.6 ";
+        final String  appName= "Version 0.7 ";
+        int totalBook=1000;
+        Book[] books = new Book[totalBook];
 
-        Book b1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010,
+        books[0] = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010,
                 296, "Greg", "9788373271890");
-        Book b2 = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch",
+        books[1] = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch",
                 2009, 352, "Helion", "9788324620845");
-        Book b3 = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide",
+        books[2] = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide",
                 "Bert Bates, Katherine Sierra", 2008, 851,
                 "McGraw-Hill Osborne Media");
 
-        b1.getInfo();
-        b2.getInfo();
-        b3.getInfo();
+        books[0].getInfo();
+        books[1].getInfo();
+        books[2].getInfo();
     }
 }
